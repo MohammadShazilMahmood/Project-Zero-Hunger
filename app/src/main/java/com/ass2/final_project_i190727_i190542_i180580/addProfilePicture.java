93c38804d32e2618f87b752fdd13a9822f1f17ef
Mohammad Shazil Mahmood <shazil2010.info@gmail.com>
2022-11-06 03:01:41 +0500

    Profile Picture Added to Cloud Storage, URL added to Realtime DB at Sign Up

--- app/src/main/java/com/ass2/final_project_i190727_i190542_i180580/addProfilePicture.java
+++ app/src/main/java/com/ass2/final_project_i190727_i190542_i180580/addProfilePicture.java
@@ -1,23 +1,43 @@
 package com.ass2.final_project_i190727_i190542_i180580;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class addProfilePicture extends AppCompatActivity {
-    ImageView selectPicture, profilePicture;
+    ImageView selectPicture, profilePicture, next;
     Uri image;
+    FirebaseAuth mAuth;
+    DatabaseReference mDatabase;
+    boolean imageSelected=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_profile_picture);
         selectPicture=findViewById(R.id.select_profile_pic);
         profilePicture=findViewById(R.id.profilePicture);
+        next=findViewById(R.id.continue_profile_pic);
+        mAuth= FirebaseAuth.getInstance();
+        mDatabase= FirebaseDatabase.getInstance().getReference();
 
         selectPicture.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -28,6 +48,54 @@ public class addProfilePicture extends AppCompatActivity {
                 startActivityForResult(intent, 23);
             }
         });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean valid=true;
+
+                if (imageSelected==false)
+                {
+                    Toast.makeText(addProfilePicture.this, "Image not selected", Toast.LENGTH_SHORT).show();
+                    valid=false;
+                }
+
+                if (valid)
+                {
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    String userID = user.getUid().toString();
+
+                    FirebaseStorage storage= FirebaseStorage.getInstance();
+                    StorageReference ref=storage.getReference().child("profile_pictures/"+userID+"/dp.jpg");
+                    ref.putFile(image).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Task<Uri> task=taskSnapshot.getStorage().getDownloadUrl();
+                            task.addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+//                                    FirebaseUser user = mAuth.getCurrentUser();
+//                                    String userID = user.getUid().toString();
+                                    mDatabase.child("users").child(userID).child("profile_picture").setValue(uri.toString());
+
+                                    Toast.makeText(addProfilePicture.this, "Profile Picture Uploaded", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(addProfilePicture.this, "Failed to upload", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+//                    mDatabase.child("users").child(userID).child("profile_information").setValue(profile);
+
+//                    Toast.makeText(addProfilePicture.this, "Profile Picture Uploaded", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
 
     @Override
@@ -37,6 +105,7 @@ public class addProfilePicture extends AppCompatActivity {
         {
             image=data.getData();
             profilePicture.setImageURI(image);
+            imageSelected=true;
         }
 
 //        if (requestCode==24 & resultCode==RESULT_OK)
@@ -46,4 +115,6 @@ public class addProfilePicture extends AppCompatActivity {
 //        }
 
     }
+
+
 }
